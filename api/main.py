6b652c50b1b65e6eb76a6f47e856bb911ec5c3f2
David Harper <real.david.harper@gmail.com>
2023-05-26 03:47:35 -0700

    scaffolding for yaml description processing

--- api/main.py
+++ api/main.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from yaml import safe_load
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def discovery():
+    with open("api.yaml", "r") as file:
+        return safe_load(file.read())
+
+@app.route("/people", methods = ["post"])
+def create_person():
+    return { "foo": "foo" }, 201
 
 app.run(host="0.0.0.0", debug=True)
