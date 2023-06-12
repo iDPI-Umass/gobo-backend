c5f63615f95e411c266e821b5bdae8a1027d2243
David Harper <real.david.harper@gmail.com>
2023-06-12 14:19:49 -0700

    lifting shared components from the API container

---
+++ worker/main.py
@@ -0,0 +1,23 @@
+# Load configuration
+from dotenv import load_dotenv
+load_dotenv()
+
+
+# Configure GOBO logging
+from logging import config
+import logging
+config.dictConfig({
+    "version": 1,
+    "formatters": {"default": {
+      "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s"
+    }},
+    "handlers": {"wsgi": {
+        "class": "logging.StreamHandler",
+        "stream": "ext://flask.logging.wsgi_errors_stream",
+        "formatter": "default"
+    }},
+    "root": {
+        "level": "INFO",
+        "handlers": ["wsgi"]
+    }
+})
