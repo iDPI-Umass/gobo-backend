2dc86f632f31220ef99a7714dc87140adfbd86de
David Harper <real.david.harper@gmail.com>
2023-08-24 18:57:21 -0700

    improvements to rate limiting behavior of subreddit view-new JSON reader

--- clients/gobo_reddit.py
+++ clients/gobo_reddit.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import time
 import json
 import httpx
 import joy
@@ -12,20 +14,30 @@ class GOBOReddit():
         output = []
 
         url = f"https://www.reddit.com/r/{subreddit}/new.json?limit=100"
+        headers = {
+            "User-Agent": os.environ.get("REDDIT_USER_AGENT")
+        }
 
-        with httpx.Client() as client:
-            r = client.get(url)
-            if r.status_code == 200:
-                body = r.json()
-                if body.get("data", None) is None:
-                    logging.warning(f"Reddit: Fetching posts for {subreddit} but response did not include data")
-                    logging.warning(body)
+        while True:
+            with httpx.Client() as client:
+                r = client.get(url, headers = headers)
+                if r.status_code == 200:
+                    body = r.json()
+                    if body.get("data", None) is None:
+                        logging.warning(f"Reddit: Fetching posts for {subreddit} but response did not include data")
+                        logging.warning(body)
+                    else:
+                        for post in body["data"]["children"]:
+                            output.append(post["data"])
+                    return output
+                
+                elif r.status_code == 429:
+                    timeout = r.headers.get("X-Ratelimit-Reset", 3)
+                    timeout = int(timeout) + 5
+                    time.sleep(timeout)
+                    continue
+                
                 else:
-                    for post in body["data"]["children"]:
-                        output.append(post["data"])
-            else:
-                logging.warning(f"Reddit: fetching posts for {subreddit} responded with status {r.status_code}")
-                logging.warning(r.json())
-
-        return output
-        
+                    logging.warning(f"Reddit: fetching posts for {subreddit} responded with status {r.status_code}")
+                    logging.warning(r.json())
+                    return output
