0f17dd8b59f60e521cefc5cb4a942f8739c307bc
David Harper <real.david.harper@gmail.com>
2023-10-02 14:42:26 -0700

    Establishing btree indexes on links and upsert queries

---
+++ db/tables/indexes.sql
@@ -0,0 +1,44 @@
+CREATE INDEX CONCURRENTLY 
+ON link
+USING btree 
+(origin_type, origin_id, target_type, target_id, name, secondary);
+
+CREATE INDEX CONCURRENTLY 
+ON link 
+USING btree 
+(target_type, target_id, origin_type, origin_id, name, secondary);
+
+CREATE INDEX CONCURRENTLY 
+ON post 
+USING btree 
+(base_url, platform_id);
+
+CREATE INDEX CONCURRENTLY 
+ON bluesky_session 
+USING btree 
+(person_id, base_url, did);
+
+CREATE INDEX CONCURRENTLY 
+ON identity 
+USING btree 
+(person_id, platform_id);
+
+CREATE INDEX CONCURRENTLY 
+ON person 
+USING btree 
+(authority_id);
+
+CREATE INDEX CONCURRENTLY 
+ON post_edge 
+USING btree 
+(identity_id, post_id, secondary);
+
+CREATE INDEX CONCURRENTLY 
+ON source 
+USING btree 
+(base_url, platform_id);
+
+CREATE INDEX CONCURRENTLY 
+ON store
+USING btree 
+(person_id, name);
