28ddf704c128b7cc5cba09b2adb4b10e36b57190
David Harper <real.david.harper@gmail.com>
2024-03-27 00:38:20 -0700

    added support for email verification gatekeeping while grandfathering in private beta testers.

--- api/authorize.py
+++ api/authorize.py
@@ -84,10 +84,41 @@ def validate_token(token):
     raise http_errors.unauthorized("Unable to find appropriate key")
 
 
-def get_roles(token):
+
+# This looks up the permissions as maintained by Auth0.
+#
+# - We need this to verify that a persona has admin access.
+# - During the private beta, we also used the permission "general" to provide
+#   general per-account level access to regular personas.
+#
+# As we move into public beta / general access, we're transitioning what
+# that "general" permission is.
+#
+# We're going to look for email verification, which is still a claim signed
+# by the authority. Then, we'll add "general" as a virtual permission.
+#
+# Benefits Of This Approach:
+# - This avoids disrupting the authorization flow outside of this function.
+# - Pre-existing accounts are gracefully handled.
+# - It's semantically consistent with the how the permission "general" is changing.
+# - If we wanted the permission to be available for federation, we're not painted
+#   into a corner; it's still based on a signed claim. So we have all our
+#   options available to us in the future.
+
+def get_permissions(token):
     claims = validate_token(token)
     g.claims = claims
-    return claims.get("permissions")
+
+    # Gets the formal permissions from the signed claim.
+    permissions = set(claims.get("permissions", []))
+
+    # We honor accounts with verified email addresses as having general access.
+    is_verified = claims.get("https://gobo.social/verified", False)
+    if is_verified == True:
+        permissions.add("general")
+    
+    return permissions
+
 
 
 def lookup_gobo_key(key): 
@@ -134,12 +165,12 @@ def authorize_request(configuration):
 
         elif parts[0] == "Bearer":
             # Relies on integration with Auth0        
-            roles = get_roles(parts[1])
+            permissions = get_permissions(parts[1])
         
-            if "admin" in roles:
+            if "admin" in permissions:
                 return
 
-            if "general" in roles and "general" in schema:
+            if "general" in permissions and "general" in schema:
                 return
 
             if "person" in schema:
