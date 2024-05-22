1be52216d12a626e356104d9ebbfa0b696399657
David Harper <real.david.harper@gmail.com>
2024-05-22 06:44:30 -0700

    supporting unpublish actions at the level of individual delivery targets.

--- api/handlers/person_delivery_target.py
+++ api/handlers/person_delivery_target.py
@@ -18,3 +18,57 @@ def person_delivery_target_get(person_id, id):
     return {
         "content": target
     }
+
+
+def unpublish_action(person_id, id):
+    target = models.delivery_target.find({
+        "person_id": person_id,
+        "id": id
+    })
+    
+    if target is None:
+        raise http_errors.not_found(
+            f"person {person_id} does not have delivery target {id}"
+        )
+    
+    # TODO: Make this more sophisticated.
+    if target["state"] != "delivered":
+        return
+
+    identity = models.identity.find({
+        "person_id": person_id,
+        "id": target["identity_id"]
+    })
+    if identity is None:
+        raise http_errors.not_found(
+            f"person {person_id} does not have identity {target["identity_id"]}"
+        )
+    if identity["stale"] == True:
+        return
+
+  
+    target["state"] = "pending"
+    target = models.delivery_target.update(target["id"], target)
+    models.task.add({
+        "queue": identity["platform"],
+        "name": "unpublish post",
+        "priority": 1,
+        "details": {
+          "target": target,
+          "identity": identity,
+        }
+    })
+
+
+def person_delivery_target_post(person_id, id):
+    action = request.json["action"]
+    
+    if action == "unpublish":
+        unpublish_action(person_id, id)
+    else:
+        raise http_errors.bad_request(
+            f"action {action} is not recognized"
+        )   
+
+    # (for now) no content response
+    return {"content": ""}
